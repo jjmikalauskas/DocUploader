20c20d01c4c72d251115603bc58a0578ed98962f
John Mikalauskas <johnmik35@hotmail.com>
2020-03-14 11:00:19 -0500

    Added in -SPA

---
+++ UploaderApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using UploaderApp.API.Models;
+
+namespace UploaderApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
